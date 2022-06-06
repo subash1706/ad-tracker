d4cb0a20f4428fabc90e3b4ae6243da14282a3b9
subash1706 <subash.somasundaram@chainsys.com>
2022-06-06 09:54:04 +0530

    commit
    Signed-off-by: subash1706 <subash.somasundaram@chainsys.com>

--- ad-tracker/src/app/main-menu/main-menu.component.ts
+++ ad-tracker/src/app/main-menu/main-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,NgForm,Validators } from '@angular/forms';
+import { FormBuilder,FormGroup} from '@angular/forms';
 import { ApiserviceService } from '../apiservice.service';
 @Component({
   selector: 'app-main-menu',
