d59e802a0d3c9dbeff266b07d5fc4618522d8d89
subash1706 <subash.somasundaram@chainsys.com>
2022-06-09 09:44:22 +0530

    commit
    Signed-off-by: subash1706 <subash.somasundaram@chainsys.com>

--- ad-tracker/src/app/signup/signup.component.ts
+++ ad-tracker/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,MinLengthValidator,NgForm,Validators } from '@angular/forms';
+import { FormBuilder,FormGroup,NgForm,Validators } from '@angular/forms';
 import { ApiserviceService } from '../apiservice.service';
 @Component({
   selector: 'app-signup',
